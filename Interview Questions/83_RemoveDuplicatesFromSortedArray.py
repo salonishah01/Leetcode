7a3c57c08bab1570431d9acd7c0a2f304d268858
Saloni Shah <saloshah01@gmail.com>
2021-04-28 08:14:03 +0530

    Create 83_RemoveDuplicatesFromSortedArray.py

---
+++ Interview Questions/83_RemoveDuplicatesFromSortedArray.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if head is None:
+            return None
+        cur = head
+        while cur.next:
+            if(cur.val == cur.next.val):
+                cur.next = cur.next.next
+            else:
+                cur = cur.next
+        return head
+        
